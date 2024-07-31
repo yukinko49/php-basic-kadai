2299279fa350cd5de135052a0d9e14607a203493
yukinko49 <yk49.skrm@gmail.com>
2024-07-31 14:37:06 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎課題</title>
+</head>
+
+<body>
+  <p>
+    <?PHP
+    $vegetable = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    print_r($vegetable);
+
+
+    ?>
+  </p>
+</body>
+
+</html>
