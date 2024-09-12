a892dcf5c14232f0e15537142818ce6ddbe6826b
yukinko49 <yk49.skrm@gmail.com>
2024-09-12 23:13:10 +0900

    Add files via upload

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+  </head>
+  <body>
+    <h2>登録が完了しました。</h2>
+    <p>社内情報がデータベースに保存されました。</p>
+    <p>
+      <button id="complete-btn" onclick="location.href='form.php'">戻る</button>
+    </p>
+  </body>
+</html>
